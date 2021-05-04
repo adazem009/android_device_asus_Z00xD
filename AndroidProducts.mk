7f9f8b1f9d7632cccb7b138a29be137e62a64820
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-05-04 20:09:57 +0200

    Switch to rr

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Z00xD.mk
+    $(LOCAL_DIR)/rr_Z00xD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Z00xD-userdebug \
-    lineage_Z00xD-eng
+    rr_Z00xD-userdebug \
+    rr_Z00xD-eng
